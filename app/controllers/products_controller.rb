fa821b2d47ecb3789016b63eae67fa8047e7dcf2
Lucy <lucymidgley13@gmail.com>
2020-03-03 20:12:07 +0000

    added some comments and reset dababase ran migratios

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,10 @@
 class ProductsController < ApplicationController
 
   def index
+    #uses MODEL
     @products = Product.all.order(created_at: :desc)
+    # View
+    #render :index is implicit!!
   end
 
   def show
