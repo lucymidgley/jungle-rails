5168fdd8d3111229ea8e3818a5fa35198422192b
Lucy <lucymidgley13@gmail.com>
2020-03-04 22:23:29 +0000

    created the totals for the number of products and number of categories and rendered a table in the dashboard for admin to show these statistics

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class Admin::DashboardController < ApplicationController
   def show
+    @total_products = Product.count(:id)
+    @total_products_quantities = Product.sum(:quantity)
+    @total_categories = Category.count(:id)
   end
 end
