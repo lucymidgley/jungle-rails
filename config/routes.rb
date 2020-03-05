2876797fa250371a158070f5d0aef998d65f632d
Lucy <lucymidgley13@gmail.com>
2020-03-05 18:27:29 +0000

    added new routes and created model for users

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,8 @@ Rails.application.routes.draw do
   # root 'welcome#index'
   # Example of regular route:
   get 'about' => 'about#show'
+  get '/signup' => 'users#new'
+    post '/users' => 'users#create'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
