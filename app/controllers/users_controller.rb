63773da1053f691b79e91ad275d24728711d3483
Lucy <lucymidgley13@gmail.com>
2020-03-05 19:53:54 +0000

    small change to form class

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,8 @@ class UsersController < ApplicationController
     end
   end
 
+  private
+
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
