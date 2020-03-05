753f4fd2092ffde91d0449f4fc06155a20527f16
Lucy <lucymidgley13@gmail.com>
2020-03-05 17:21:25 +0000

    changed the format of the route in the config file to explore how the routes work

--- config/routes.rb
+++ config/routes.rb
@@ -17,14 +17,14 @@ Rails.application.routes.draw do
     resources :products, except: [:edit, :update, :show]
   end
 
+  #resources :about, path: '/about', only: [:show]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
   # Example of regular route:
-   get 'about' => 'about#show'
+  get 'about' => 'about#show'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
