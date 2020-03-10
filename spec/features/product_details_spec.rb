11e95f9088783107d546c1211eb7d42fb9394288
Lucy <lucymidgley13@gmail.com>
2020-03-10 16:09:12 +0000

    added test for products partial

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,13 +1,12 @@
 require 'rails_helper'
 
 RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
-  
 
   # SETUP
   before :each do
     @category = Category.create! name: 'Apparel'
 
-    10.times do |n|
+    
       @category.products.create!(
         name:  Faker::Hipster.sentence(3),
         description: Faker::Hipster.paragraph(4),
@@ -15,17 +14,21 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
         quantity: 10,
         price: 64.99
       )
-    end
+   
   end
 
-  xscenario "They see all products" do
+  scenario "They can click on a product and see its show page" do
     # ACT
     visit root_path
+    click_on('Details')
 
     # DEBUG
-    save_screenshot
+    #save_screenshot
+    
 
     # VERIFY
-    expect(page).to have_css 'article.product', count: 10
+    expect(page).to have_css 'section.products-show', count: 1
+    save_screenshot
+    puts page.html
   end
 end
